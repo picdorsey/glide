d17152b56349a08ecb0b3a92924d969cdb8d18ff
Adam Dorsey <adam.dorsey@picdorsey.com>
2015-01-14 17:03:53 -0500

    Initial v1.0.0 commit.

---
+++ index.js
@@ -0,0 +1,37 @@
+/* global $: false, module, window, document, requestAnimationFrame */
+
+/*
+    Shift - A slick JS scroll-to component.
+    Copyright (c) 2015 Piccirilli Dorsey, Inc.
+ */
+
+'use strict';
+
+var TWEEN = require('tween.js');
+var getComputedStyle = require('./lib/getComputedStyle');
+
+module.exports = function(t, o) {
+    if ("undefined" != typeof window) {
+
+        var e;
+        e = null !== window.pageYOffset ? window.pageYOffset : null !== document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop;
+
+        var r = document.getElementById(t),
+            p = (parseInt(getComputedStyle(r, "margin-top"), 10), parseInt(getComputedStyle(r, "padding-top"), 10), r.offsetTop - o);
+
+        new TWEEN.Tween({
+            y: e
+        }).to({
+                y: p
+            }, 600).easing(TWEEN.Easing.Exponential.InOut).onUpdate(function() {
+                window.scrollTo(0, this.y);
+            }).start();
+
+        var animate = function(e) {
+            TWEEN.update(e);
+            requestAnimationFrame(animate);
+        };
+
+        animate();
+    }
+};
